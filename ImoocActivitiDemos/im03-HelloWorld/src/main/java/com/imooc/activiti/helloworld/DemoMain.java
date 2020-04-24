a1ac52fd21d70e85a5da1e1d1cb705c204e302aa
sunspeedzy <84886255@qq.com>
2020-04-25 01:34:34 +0800

    创建Activiti示例代码

---
+++ ImoocActivitiDemos/im03-HelloWorld/src/main/java/com/imooc/activiti/helloworld/DemoMain.java
@@ -0,0 +1,22 @@
+package com.imooc.activiti.helloworld;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 启动类
+ * 
+ * @author zhangyan
+ * @create 2020-04-25
+ */
+public class DemoMain {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoMain.class);
+	
+	public static void main(String[] args) {
+		LOGGER.info("启动我们的程序");
+		
+		
+		LOGGER.info("结束我们的程序");
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+此工程是我学习Activiti过程中，按照教程和书籍编写的demo代码。
+ImoocActivitiDemos模块中，是 慕课网《Activiti6.0工作流引擎深度解析与实战》的demo
+CrazyBookDemos模块中，是 《疯狂工作流讲义》的demo
