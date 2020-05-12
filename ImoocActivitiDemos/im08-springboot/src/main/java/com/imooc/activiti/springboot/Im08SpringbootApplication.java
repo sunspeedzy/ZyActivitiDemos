d17c771c4dab050209a0b77dce00a8e968c13084
zhangyan_g <zhangyan_g@aspirecn.com>
2020-05-12 18:59:31 +0800

    添加慕课网教程第08章集成SpringBoot2 的示例

---
+++ ImoocActivitiDemos/im08-springboot/src/main/java/com/imooc/activiti/springboot/Im08SpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.imooc.activiti.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangyan_g
+ */
+@SpringBootApplication
+public class Im08SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Im08SpringbootApplication.class, args);
+    }
+
+}
---
+++ ImoocActivitiDemos/im08-springboot/src/test/java/com/imooc/activiti/springboot/Im08SpringbootApplicationTests.java
@@ -0,0 +1,19 @@
+package com.imooc.activiti.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 测试 Activiti6.0 集成 Spring boot 2.2.0.RELEASE
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class Im08SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
